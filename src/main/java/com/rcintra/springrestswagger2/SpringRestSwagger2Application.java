8c559642676affbf87063be7f28fc29b89dc1756
Rafael Cintra <ra.cintra@gmail.com>
2017-07-18 22:24:09 -0300

    first commit

---
+++ src/main/java/com/rcintra/springrestswagger2/SpringRestSwagger2Application.java
@@ -0,0 +1,12 @@
+package com.rcintra.springrestswagger2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestSwagger2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestSwagger2Application.class, args);
+	}
+}
